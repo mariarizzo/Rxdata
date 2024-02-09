de6ebe5857f624ad6751ad5e0d6ae88146514dc2
Maria Rizzo <mrizzo@bgsu.edu>
2024-02-09 11:54:34 -0500

    add ncy.marathon and world.record.mile

---
+++ R/world.record.mile.R
@@ -0,0 +1,23 @@
+#' World Record Mile Data
+#'
+#' Mile run world record progression  as recorded by the
+#' International Amateur Athletics Federation (IAAF).  The
+#' dataset includes 32 world records for men ratified by
+#' the IAAF, and 29 world records for women both in the
+#' pre-IAAF and IAAF eras.
+#'
+#' @format 276 obs. of  3 variables:
+#' \describe{
+#' \item{Gender}{female or male}
+#' \item{Time}{chr: time as "mm:ss"}
+#' \item{X}{The whole minutes part of Time}
+#' \item{X.1}{The "ss" part of Time}
+#' \item{seconds}{time expressed in seconds}
+#' \item{Athlete}{Name}
+#' \item{Nationality}{nationality}
+#' \item{Date}{chr: date}
+#' \item{Year}{year}
+#' }
+#' @source Wikipedia page \url{https://en.wikipedia.org/wiki/Mile_run_world_record_progression}
+"world.record.mile"
+
