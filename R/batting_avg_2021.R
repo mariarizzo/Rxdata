57c276fa800ee33840fd8ca79c01bbed99118354
Maria Rizzo <mrizzo@bgsu.edu>
2024-03-10 20:04:30 -0400

    add two datasets

---
+++ R/batting_avg_2021.R
@@ -0,0 +1,13 @@
+#' Batting Averages 2021
+#'
+#'
+#' @format 231 obs. of 5 variables:
+#' \describe{
+#' \item{Player}{Name of player}
+#' \item{lgID}{League}
+#' \item{H}{Hits}
+#' \item{AB}{At bats}
+#' \item{AVG}{Batting average}
+#' }
+#'
+"batting_avg_2021"
---
+++ R/four_players.R
@@ -0,0 +1,19 @@
+#' Four Players Home Plate Statistics
+#'
+#'
+#' @format 64 obs. of 12 variables:
+#' \describe{
+#' \item{PX}{chr: ?}
+#' \item{PZ}{chr: ?}
+#' \item{BIP}{?}
+#' \item{H}{?}
+#' \item{HR}{?}
+#' \item{H_Rate}{?}
+#' \item{HR_Rate}{?}
+#' \item{Z_H}{?}
+#' \item{Z_HR}{?}
+#' \item{Player}{chr: Player name}
+#' \item{px}{?}
+#' }
+#'
+"four_players"
